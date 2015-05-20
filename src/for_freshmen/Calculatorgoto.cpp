78a3b6b92ca7f3aa8b27ffcfef133ff43cede0da
GotoRyohei <s1526051yc@s.chibakoudai.jp>
2015-05-20 23:26:07 +0900

    四則演算電卓 goto #5

---
+++ src/for_freshmen/Calculatorgoto.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+main()
+{
+	char ope;
+	double a, b, ans;
+	
+	while (1) {
+		fflush(stdin);
+		a = 0;
+		b = 0;
+		ans = 0;
+		
+		printf("Ž®‚ð“ü—Í‚µ‚Ä‚­‚¾‚³‚¢ :");
+		scanf("%lf %c %lf", &a, &ope, &b);
+		
+		switch (ope) {
+			case '+' :
+				ans = a + b;
+				break;
+			case '-' :
+				ans = a - b;
+				break;
+			case '*' :
+				ans = a * b;
+				break;
+			case '/' :
+				if (b == 0) {
+					printf("0‚ÅŠ„‚é‚±‚Æ‚Í‚Å‚«‚Ü‚¹‚ñ\n");
+					continue;
+				}
+				ans = a / b;
+				break;
+			default:
+				printf("ERROR\n");
+				continue;
+		}
+		
+		printf("“š‚¦ :%lf\n", ans);
+	}
+}
