c05e7acb9ee9194e20484eed52dad6148f9eed84
Youta Seki <uyu9480s@outlook.jp>
2015-06-05 23:11:12 +0900

    円を描画する関数の追加

--- src/for_1426/ImageProcessing/otamesi_hist.cpp
+++ src/for_1426/ImageProcessing/otamesi_hist.cpp
@@ -5,11 +5,17 @@
 const char *filename="142.png";
 
 void mouse(int event,int x,int y,int flags,void*param);
+void drawCircle(IplImage *img, int radius,CvPoint center);
 
 
 int main(){
 	IplImage *img =0;
 	img=cvLoadImage(filename,CV_LOAD_IMAGE_COLOR);
+	
+	CvPoint center = cvPoint(img->width/2, img->height/2);
+	int radius = 100;
+	drawCircle(img ,radius ,center);
+
 	cvNamedWindow("Picture",CV_WINDOW_AUTOSIZE);
 	cvShowImage("Picture",img);
 	cvSetMouseCallback("Picture",mouse);
@@ -34,4 +40,13 @@ void mouse (int event,int x, int y,int flags,void *param=NULL)
 		default:
 			break;
 	}
+}
+
+void drawCircle (IplImage *img, int radius, CvPoint center){
+
+	CvScalar color = CV_RGB(255 ,0 ,0);
+	int thickness = 1;
+	int linetype = 8;
+	int shift = 3;
+	cvCircle(img ,center, radius, color, thickness, linetype, shift);
 }
