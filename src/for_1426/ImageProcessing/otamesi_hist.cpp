4d3b76bee87f7f929562c4df219b5234848e68b7
tilly1113 <chisato.rukia@gmail.com>
2015-06-05 22:24:42 +0900

    クリックした点の座標の取得、表示

--- src/for_1426/ImageProcessing/otamesi_hist.cpp
+++ src/for_1426/ImageProcessing/otamesi_hist.cpp
@@ -4,11 +4,34 @@
 
 const char *filename="142.png";
 
-void main(void)
-  {
-    IplImage *img = 0;
-	img = cvLoadImage(filename,CV_LOAD_IMAGE_COLOR);
+void mouse(int event,int x,int y,int flags,void*param);
+
+
+int main(){
+	IplImage *img =0;
+	img=cvLoadImage(filename,CV_LOAD_IMAGE_COLOR);
 	cvNamedWindow("Picture",CV_WINDOW_AUTOSIZE);
-    cvShowImage("Picture",img);
-    cvWaitKey(0);
-  }
+	cvShowImage("Picture",img);
+	cvSetMouseCallback("Picture",mouse);
+	cvWaitKey(0);
+	cvReleaseImage(&img);
+	cvDestroyWindow("Picture");
+
+	return 0;
+}
+void mouse (int event,int x, int y,int flags,void *param=NULL)
+{
+	switch(event)
+	{
+		case CV_EVENT_LBUTTONDOWN:
+			std::cout<<x<<","<<y<<"\n";
+			break;
+		
+		case CV_EVENT_RBUTTONDOWN:
+			std::cout<<x<<","<<y<<"\n";
+			break;
+
+		default:
+			break;
+	}
+}
